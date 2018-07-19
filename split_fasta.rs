02aaf264f3583dc81731af1c8353da24d1f4803a
muchomacho <tosh.un316@gmail.com>
2018-07-20 00:38:50 +0900

    revise

--- split_fasta.rs
+++ split_fasta.rs
@@ -21,18 +21,18 @@ fn main() {
     if bytes == 0 || line[0] != b'<' {
         panic!("input file is not a fasta file.");
     }
-    loop {
-        if bytes == 0 {
-            break;
-        }
+    'outer: loop {
         let write_f = File::create(String::from_utf8_lossy(&line[1..(line.len() - 1)]).to_string() + ".fasta").unwrap();
         let mut writer = BufWriter::new(write_f);
         writer.write(&line[..]).unwrap();
         line.clear();
         loop {
             bytes = reader.read_until(b'\n', &mut line).unwrap();
-            if bytes == 0 || line[0] == b'<' {
-                break;
+            if bytes == 0 {
+                break 'outer;
+            }
+            if line[0] == b'<' {
+                continue 'outer;
             }
             writer.write(&line[..]).unwrap();
             line.clear();
