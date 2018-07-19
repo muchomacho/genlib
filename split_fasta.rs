0f0e1e747747eb6fc5ece3cfb448557a8018760f
muchomacho <tosh.un316@gmail.com>
2018-07-19 23:57:40 +0900

    created fasta split script

---
+++ split_fasta.rs
@@ -0,0 +1,45 @@
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, Write, BufReader, BufWriter};
+#[allow(unused_imports)]
+use std::io::prelude::*;
+#[allow(unused_imports)]
+use std::fs::File;
+#[allow(unused_imports)]
+use std::env;
+
+// This script divides one fasta file into multiple files by headers
+
+fn main() {
+    let input = get_param();
+    let f = File::open(input).unwrap();
+    let mut reader = BufReader::new(f);
+
+    let mut bytes;
+    let mut line = Vec::new();
+    bytes = reader.read_until(b'\n', &mut line).unwrap();
+    if line[0] != b'<' {
+        panic!("input file is not a fasta file.");
+    }
+    loop {
+        if bytes == 0 {
+            break;
+        }
+        let write_f = File::create(String::from_utf8_lossy(&line[1..(line.len() - 1)]).to_string() + ".fasta").unwrap();
+        let mut writer = BufWriter::new(write_f);
+        writer.write(&line[..]).unwrap();
+        line.clear();
+        loop {
+            bytes = reader.read_until(b'\n', &mut line).unwrap();
+            if bytes == 0 || line[0] == b'<' {
+                break;
+            }
+            writer.write(&line[..]).unwrap();
+            line.clear();
+        }
+    }
+}
+
+fn get_param() -> String {
+    let args: Vec<String> = env::args().collect();
+    return args.into_iter().nth(1).unwrap();
+}
