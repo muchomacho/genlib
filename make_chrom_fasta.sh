7585b0b0d5ce02f60467248c7d516597a333c001
muchomacho <tosh.un316@gmail.com>
2018-07-20 14:08:36 +0900

    fix bugs

--- make_chrom_fasta.sh
+++ make_chrom_fasta.sh
@@ -3,25 +3,28 @@
 # This script makes fasta file which only contains chromosome sequence from human reference fasta file
 # Usage: ./make_chrom_fasta.sh [-r reference_fasta_file] [-o output_fasta_file]
 shopt -s extglob
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 while getopts "r:o:" opt; do
     case $opt in
-	r) reference_fasta_file=$OPTARG ;;
-    o) output_fasta_file=$OPTARG ;;
+	r) REF=$OPTARG ;;
+    o) OUT=$OPTARG ;;
 	[?]) echo "Usage: ${0##*/} [-r reference_fasta_file] [-o output_fasta_file]";exit ;;
     esac
 done
 
-cd $DIR
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+REF_DIR="$( cd "$( dirname "${REF}" )" >/dev/null && pwd )"
+OUT_DIR="$( cd "$( dirname "${OUT}" )" >/dev/null && pwd )"
+
+cd $SRC_DIR
 rustc -O split_fasta.rs
 mkdir _tmpdir
 cd _tmpdir
-../split_fasta reference_fasta_file
+../split_fasta ${REF_DIR}/$( basename reference_fasta_file)
 
-for num in `seq 22` "X" "Y"
+for num in $( seq 22 ) "X" "Y"
 do
-    cat chr${num}.fasta >> output_fasta_file
+    cat chr${num}.fasta >> ${OUT_DIR}/$( basename output_fasta_file)
 done
 
 cd ..
