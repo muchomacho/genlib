8143d6d3eeb60cabd6e7023f3172d3b11922b472
muchomacho <tosh.un316@gmail.com>
2018-07-20 11:35:49 +0900

    revise fasta comment line handling

--- split_fasta.rs
+++ split_fasta.rs
@@ -7,31 +7,72 @@ use std::fs::File;
 #[allow(unused_imports)]
 use std::env;
 
-// This script divides one fasta file into multiple files by headers
+// This script divides one fasta file into multiple files by the sequence
 // Used to create a fasta file which only contains 24 chromosome information
 
 fn main() {
+    // open target fasta file
     let input = get_param();
     let f = File::open(input).unwrap();
     let mut reader = BufReader::new(f);
 
+    // read file by the line
     let mut bytes;
     let mut line = Vec::new();
     bytes = reader.read_until(b'\n', &mut line).unwrap();
-    if bytes == 0 || line[0] != b'<' {
-        panic!("input file is not a fasta file.");
+    if line[0] != b'>' {
+        panic!("invalid comment line");
     }
     'outer: loop {
+        // modify comment line so that it can become a valid file name
+        // delete delimiters at both side
+        if bytes <= 2 {
+        panic!("invalid comment line");
+        }
+        let mut del_indices = Vec::new();
+        for i in 1..(line.len() - 1) {
+            if is_delim(line[i]) {
+                del_indices.push(i);
+            } else {
+                break;
+            }
+        } 
+        for i in (1..(line.len() - 1)).rev() {
+            if is_delim(line[i]) {
+                del_indices.push(i);
+            } else {
+                break;
+            }
+        }
+        del_indices.sort();
+        del_indices.dedup();
+        del_indices.reverse();
+        for &i in del_indices.iter() {
+            line.remove(i);
+        }
+        if line.len() <= 2 {
+            panic!("invalid comment line");
+        }
+        // transform internal delimiters into '_' 
+        for i in 1..(line.len() - 1) {
+            if is_delim(line[i]) {
+                line[i] = b'_';
+            }
+        }
+
+        // create a file and write sequence
         let write_f = File::create(String::from_utf8_lossy(&line[1..(line.len() - 1)]).to_string() + ".fasta").unwrap();
         let mut writer = BufWriter::new(write_f);
         writer.write(&line[..]).unwrap();
         line.clear();
         loop {
             bytes = reader.read_until(b'\n', &mut line).unwrap();
+            // EOF of input file
             if bytes == 0 {
                 break 'outer;
             }
-            if line[0] == b'<' {
+            // new sequence
+            if line[0] == b'>' {
                 continue 'outer;
             }
             writer.write(&line[..]).unwrap();
@@ -44,3 +85,5 @@ fn get_param() -> String {
     let args: Vec<String> = env::args().collect();
     return args.into_iter().nth(1).unwrap();
 }
+
+fn is_delim(b: u8) -> bool { b == b'\n' || b == b'\r' || b == b'\t' || b == b' ' || b == b';' }
