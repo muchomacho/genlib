1c54bef0b52b33c0712fefb0e0f1b219c257ec72
muchomacho <tosh.un316@gmail.com>
2018-07-26 20:55:06 +0900

    simplify code

--- split_fasta.rs
+++ split_fasta.rs
@@ -1,17 +1,24 @@
-#[allow(unused_imports)]
-use std::io::{stdin, stdout, Write, BufReader, BufWriter};
-#[allow(unused_imports)]
-use std::io::prelude::*;
+// This script divides one fasta file into multiple files by the sequence
+// Used to create a fasta file which only contains 24 chromosome information
+
+// Todo: 
+// use String and use trim to delete leading and trailing whitespaces 
+// use writeln! to write to buffer
+
 #[allow(unused_imports)]
 use std::fs::File;
 #[allow(unused_imports)]
 use std::env;
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, Write, BufReader, BufWriter};
+#[allow(unused_imports)]
+use std::io::prelude::BufRead;
 
-// todo: use trim to delete leading and trailing whitespaces 
-// This script divides one fasta file into multiple files by the sequence
-// Used to create a fasta file which only contains 24 chromosome information
-
+// read/write buffer size
 static BUF_SIZE: usize = 1024 * 1024 * 10;
+// buffer size for line
+// assume that only ASCII characters are contained in the input FASTA file
+static LINE_SIZE: usize = 150;
 
 fn main() {
     // open target fasta file
@@ -20,64 +27,44 @@ fn main() {
     let mut reader = BufReader::with_capacity(BUF_SIZE, f);
 
     // read file by the line
-    let mut bytes;
-    let mut line = Vec::new();
-    bytes = reader.read_until(b'\n', &mut line).unwrap();
-    if line[0] != b'>' {
+    let mut num_bytes;
+    let mut line = Vec::with_capacity(LINE_SIZE);
+    num_bytes = reader.read_until(b'\n', &mut line).unwrap();
+    if line[0] != b'<' {
         panic!("invalid comment line");
     }
+
+    // comment line process
     'outer: loop {
-        // modify comment line so that it can become a valid file name
-        // delete delimiters at both side
-        if bytes <= 2 {
-        panic!("invalid comment line");
-        }
-        let mut del_indices = Vec::new();
-        for i in 1..(line.len() - 1) {
-            if is_delim(line[i]) {
-                del_indices.push(i);
-            } else {
-                break;
-            }
-        } 
-        for i in (1..(line.len() - 1)).rev() {
-            if is_delim(line[i]) {
-                del_indices.push(i);
-            } else {
-                break;
-            }
-        }
-        del_indices.sort();
-        del_indices.dedup();
-        del_indices.reverse();
-        for &i in del_indices.iter() {
-            line.remove(i);
-        }
-        if line.len() <= 2 {
+        if num_bytes <= 2 {
             panic!("invalid comment line");
         }
-        // transform internal delimiters into '_' 
-        for i in 1..(line.len() - 1) {
-            if is_delim(line[i]) {
-                line[i] = b'_';
-            }
-        }
-
-        // create a file and write sequence
-        let write_f = File::create(String::from_utf8_lossy(&line[1..(line.len() - 1)]).to_string() + ".fasta").unwrap();
+        // modify comment line so that it can become a valid file name
+        let mut file_name = String::from_utf8_lossy(&line[..]).into_owned();
+        file_name = file_name
+            .replace("<", "")
+            .trim()
+            .replace(" ", "_")
+            .replace("\t", "_")
+            .replace(";", "_")
+            + ".fasta";
+        // create a split FASTA file
+        let write_f = File::create(file_name).unwrap();
         let mut writer = BufWriter::with_capacity(BUF_SIZE, write_f);
+        // sequence line process
+        // read/write a sequence by the line
         writer.write(&line[..]).unwrap();
         line.clear();
         loop {
-            bytes = reader.read_until(b'\n', &mut line).unwrap();
+            num_bytes = reader.read_until(b'\n', &mut line).unwrap();
             // EOF of input file
-            if bytes == 0 {
-                let _ok = writer.flush().unwrap();
+            if num_bytes == 0 {
+                writer.flush().unwrap();
                 break 'outer;
             }
-            // new sequence
-            if line[0] == b'>' {
-                let _ok = writer.flush().unwrap();
+            // new comment line
+            if line[0] == b'<' {
+                writer.flush().unwrap();
                 continue 'outer;
             }
             writer.write(&line[..]).unwrap();
@@ -86,9 +73,8 @@ fn main() {
     }
 }
 
+
 fn get_param() -> String {
     let args: Vec<String> = env::args().collect();
     return args.into_iter().nth(1).unwrap();
 }
-
-fn is_delim(b: u8) -> bool { b == b'\r' || b == b'\t' || b == b' ' || b == b';' }
