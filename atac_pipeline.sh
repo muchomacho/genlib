b388a2cc021dc0ff3c7877b69c5f13d0d31858f6
muchomacho <tosh.un316@gmail.com>
2018-07-30 16:19:56 +0900

    create

---
+++ atac_pipeline.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+shopt -s extglob
+
+while getopts "d:r:" opt; do
+    case $opt in
+    d) dir=$OPTARG ;;
+	r) reference_fasta_file=$OPTARG ;;
+	[?]) echo "Usage: ${0##*/} [-d sequence file directory] [-r reference_fasta_file] ";exit ;;
+    esac
+done
+
+cd dir
+mkdir _tmp
+cd _tmp
+for file in $( ls ../*_*_L001_* ); do
+    cat ../${file} ../$( echo ${file} | sed s/L001/L002/ ) > $( echo ${file} | sed s/_L001// )
+done
+
+for file in $( ls *_*_R1_* ); do
+    ~/bwa/bwa mem -t 32 reference_fasta_file ${file} $( echo ${file} | sed s/R1/R2/ ) > $( echo ${file} | sed s/_R1// ).sam
+done
+
+for file in $( ls *.sam ); do
+    samtools-1.3.1 view -Su ${file} | samtools-1.3.1 sort - > $( echo ${file} | sed s/sam/bam/ )
+done
+
+ls *.bam > bam_list.txt
+samtools-1.3.1 merge -b bam_list.txt ../merged.bam
+
+cd ..
+bedtools bamtobed -i merged.bam > merged.bed
+
+rm -r _tmp
+
+
+    
