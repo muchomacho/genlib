67745450b921e3e8f7d1968d696bd169943dccc5
muchomacho <tosh.un316@gmail.com>
2018-10-30 16:09:38 +0900

    Fix same bug

--- extract_diff_region.py
+++ extract_diff_region.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
             total_reads2 += np.sum(count)
             counts2.append(count)
         assert len(counts1[i - 1]) == len(counts2[i - 1]), "the lengths of two array must be equal."
-        total_test += len(counts1[i])
+        total_test += len(counts1[i - 1])
     
     results = []
     for i in range(1, 25):
