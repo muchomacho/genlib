be30fad28a33d6c197c998d3e49ad59fd90ff61c
muchomacho <tosh.un316@gmail.com>
2018-11-13 17:40:59 +0900

    Fix bugs

--- get_gene_regions.sh
+++ get_gene_regions.sh
@@ -4,4 +4,4 @@
 # output: standard output
 # Usage: get_gene_regions.sh <gene count file> <gene bed file> 
 
-grep -f $1 $2 | awk -F'\t' '{print $3 "\t" $5 "\t" $6 }'
+grep -w -f $1 $2 | awk -F'\t' '{print $3 "\t" $5 "\t" $6 }'
