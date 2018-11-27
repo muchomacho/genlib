17e3b8dc558be896238090106ac5272492d58410
muchomacho <tosh.un316@gmail.com>
2018-11-27 18:25:00 +0900

    Fix comment

--- get_gene_regions.sh
+++ get_gene_regions.sh
@@ -2,6 +2,6 @@
 
 # This script extracts genomic regions of target genes by bed format
 # output: standard output
-# Usage: get_gene_regions.sh <gene count file> <gene bed file> 
+# Usage: get_gene_regions.sh <gene list file> <gene bed file> 
 
 grep -w -f $1 $2 | awk -F'\t' '{print $3 "\t" $5 "\t" $6 }'
